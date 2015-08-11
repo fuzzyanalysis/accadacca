f3d51086f92d7358fbf2c2259483c7cd0b87a7a0
fuzzyanalysis <sh4ngithub>
2015-08-11 23:11:52 +1000

    frank - further refinements

--- src/main/java/com/fuzzyanalysis/accadacca/view/MakeDropdownView.java
+++ src/main/java/com/fuzzyanalysis/accadacca/view/MakeDropdownView.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIViewRoot;
